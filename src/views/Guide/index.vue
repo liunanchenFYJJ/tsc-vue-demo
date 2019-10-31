29750d01b33ccb66a8e751a6d9b1d0957ab97bee
liunanchenFYJJ <liunan_chen@163.com>
2019-10-31 21:21:11 +0800

    update

--- src/views/Guide/index.vue
+++ src/views/Guide/index.vue
@@ -4,13 +4,14 @@
  * @Author: https://github.com/liunanchenFYJJ
  * @Date: 2019-10-30 10:49:12
  * @LastEditors: https://github.com/liunanchenFYJJ
- * @LastEditTime: 2019-10-30 20:08:09
+ * @LastEditTime: 2019-10-31 09:12:25
  -->
 <template>
   <div>
     <div>模拟页面分布引导</div>
     <!-- TODO: 重要 .prevent.stop 否则出现闪现 -->
-    <el-button @click.prevent.stop="startGuide">开始指引</el-button>
+    <!-- 阻止单击事件继续传播 -->
+    <el-button @click.stop="startGuide">开始指引</el-button>
   </div>
 </template>
 <script>
@@ -18,7 +19,6 @@ import Driver from 'driver.js';
 import 'driver.js/dist/driver.min.css';
 import steps from './steps';
 
-const driver = new Driver();
 export default {
   name: 'Guide',
   data() {
@@ -27,7 +27,9 @@ export default {
     };
   },
   mounted() {
-    this.driver = new Driver();
+    this.driver = new Driver({
+      allowClose: false,
+    });
   },
   methods: {
     startGuide() {
