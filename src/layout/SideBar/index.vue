264c98e8022e9aaaa20276c455690a1bafb62948
LiunanChen <liunan_chen@163.com>
2019-11-06 00:16:24 +0800

    clipboard

--- src/layout/SideBar/index.vue
+++ src/layout/SideBar/index.vue
@@ -44,9 +44,9 @@
           <el-menu-item index="1-4-1">选项1</el-menu-item>
         </el-submenu>
       </el-submenu>
-      <el-menu-item index="2">
+      <el-menu-item index="/clipboard">
         <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+        <span slot="title">剪切板</span>
       </el-menu-item>
       <el-menu-item index="/guide">
         <i class="el-icon-setting"></i>
---
+++ src/views/Clipboard/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    剪切板
+    <br>
+    <el-input :value="msg" class="input-with-select">
+      <el-button id="btn" slot="append" icon="el-icon-edit" v-popover:popover2 @click="handleClip($event, msg)"></el-button>
+    </el-input>
+    <!--  -->
+    <el-popover
+      ref="popover2"
+      placement="bottom"
+      trigger="click"
+      content="复制成功！">
+    </el-popover>
+  </div>
+</template>
+<script>
+import ClipboardJS from 'clipboard';
+
+export default {
+  name: 'Clipboard',
+  data() {
+    return {
+      msg: '我很帅!',
+    };
+  },
+  methods: {
+    handleClip(e, msg) {
+      console.log(e);
+      console.log(e.target);
+      let c = new ClipboardJS('#btn');
+    },
+  },
+};
+</script>
